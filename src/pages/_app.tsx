b26a525960b50d3ddeeadcb850cc8d379c112bee
jimitas <nonajun805@gmail.com>
2023-07-16 11:17:15 +0900

    投稿したつぶやきを配列に格納する。 ただし、型エラーあり

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navbar from "@/Components/Navbar";
+import { AuthProvider } from "@/Context/auth";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <div>
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+    </AuthProvider>
+  );
 }
