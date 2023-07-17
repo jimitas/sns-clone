fd4351f0056efb9572c83899744fca67941132b7
jimitas <nonajun805@gmail.com>
2023-07-17 13:36:58 +0900

    main

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:5000/api",
+  // baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
